0cc1d3cb243391f184740805cb8e44ffe82c7329
MartaPiatti <marta.piatti@mail.polimi.it>
2022-11-23 03:47:41 +0100

    Assignment

--- sketch.js
+++ sketch.js
@@ -1,16 +1,50 @@
+const urlString = window.location.href;
+
+let url = new URL(urlString);
+
+let robotoBold, robotoRegular, sansita;
+
 function preload() {
-	// put preload code here
+  robotoBold = loadFont("assets/Roboto/Roboto-Bold.ttf");
+  robotoRegular = loadFont("assets/Roboto/Roboto-Regular.ttf");
+  sansita = loadFont("assets/Sansita/Sansita-ExtraBold.ttf");
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(windowWidth, windowHeight);
+  background("#ffbf6e");
+  textSize(60);
+  fill("white");
+  textFont(sansita);
+  textAlign(CENTER);
+  text("CONNECT THE DOTS", windowWidth / 2, 485);
+  textSize(30);
+  fill("#854008");
+  textFont(robotoRegular);
+  text("Use the cursors to change the tip color", windowWidth / 2, 535);
+  text("Tilt your iPad to draw and shake it to restart", windowWidth / 2, 575);
+  let enter = createButton("START DRAWING");
+  enter.size(320, 50);
+  enter.position(250, 645);
+  enter.mousePressed(changePage);
+  enter.style("font-family", robotoBold);
+  enter.style("background-color", "#f67f00");
+  enter.style("color", "#ffffff");
+  enter.style("font-size", "30px");
+  enter.style("text-align", "center");
+  enter.style("font-weight", "bold");
+  enter.style("border", "none");
+  enter.style("border-radius", "4px");
 }
 
-function draw() {
-	// put drawing code here
+function draw() {}
+
+function changePage() {
+  window.open(url + "first.html", "_self");
+}
+
+function touchEnded(event) {
+  if (DeviceOrientationEvent && DeviceOrientationEvent.requestPermission) {
+    DeviceOrientationEvent.requestPermission();
+  }
 }
---
+++ sketch1.js
@@ -0,0 +1,129 @@
+let tip, r, g, b, nBox, imgC, imgF, rText, gText, bText, cx, cy;
+let robotoBold,
+  kitty,
+  fish,
+  i = 0;
+
+//upload the fonts and images
+function preload() {
+  robotoBold = loadFont("assets/Roboto/Roboto-Bold.ttf");
+  imgC = loadImage("assets/cat.png");
+  imgF = loadImage("assets/fish.png");
+}
+
+function setup() {
+  setShakeThreshold(20);
+  createCanvas(windowWidth, windowHeight);
+  image(imgC, 0, 0, 820, 1180);
+  //create html elemnts: sliders
+  r = createSlider(0, 255, 0);
+  r.position(20, 20);
+  r.addClass("mySliders");
+  g = createSlider(0, 255, 0);
+  g.position(20, 50);
+  g.addClass("mySliders");
+  b = createSlider(0, 255, 0);
+  b.position(20, 80);
+  b.addClass("mySliders");
+  //create button. When pressed the function setKitty is activated
+  kitty = createButton("Cat");
+  kitty.size(75, 30);
+  kitty.position(20, 130);
+  kitty.mousePressed(setKitty);
+  kitty.style("font-family", robotoBold);
+  kitty.style("background-color", "#854008");
+  kitty.style("color", "#ffffff");
+  kitty.style("font-size", "20px");
+  kitty.style("text-align", "center");
+  kitty.style("font-weight", "bold");
+  kitty.style("border", "none");
+  kitty.style("border-radius", "4px");
+  //create button. When pressed the function setFish is activated
+  fish = createButton("Fish");
+  fish.size(75, 30);
+  fish.position(120, 130);
+  fish.mousePressed(setFish);
+  fish.style("font-family", robotoBold);
+  fish.style("background-color", "#f67f00");
+  fish.style("color", "#ffffff");
+  fish.style("font-size", "20px");
+  fish.style("text-align", "center");
+  fish.style("font-weight", "bold");
+  fish.style("border", "none");
+  fish.style("border-radius", "4px");
+  //set the position of the tip
+  cx = 284;
+  cy = 623;
+  fill("#f67f00");
+  tip = circle(cx, cy, 5);
+}
+
+function draw() {
+  create();
+}
+
+//create the function create o it can be activated also at different moments
+function create() {
+  noStroke();
+  fill("#dedede");
+  rect(0, 0, 220, 185);
+  //get the values from the sliders to change the color of the tip
+  const rValue = r.value();
+  const gValue = g.value();
+  const bValue = b.value();
+  textFont(robotoBold);
+  fill("red");
+  rText = text("R: " + rValue, r.x * 2 + r.width, 35);
+  fill("green");
+  gText = text("G: " + gValue, g.x * 2 + g.width, 65);
+  fill("blue");
+  bText = text("B: " + bValue, b.x * 2 + b.width, 95);
+  //change the tip position following the rotation of the device and mapping it to control better the value
+  const dx = map(rotationY, -90, 90, -1, 1);
+  const dy = map(rotationX, -180, 180, -1, 1);
+  fill(rValue, gValue, bValue);
+  tip = circle(cx, cy, 5);
+  cx += dx * 2.5;
+  cy += dy * 2.5;
+  //puts a limit to its value
+  cx = constrain(cx, 0, width);
+  cy = constrain(cy, 0, height);
+}
+
+//when cat is pressed to change drawing
+function setKitty() {
+  image(imgC, 0, 0, width, height);
+  cx = 284;
+  cy = 623;
+  i = 0;
+  create();
+  kitty.style("background-color", "#854008");
+  fish.style("background-color", "#f67f00");
+}
+
+//when fish is pressed to change drawing
+function setFish() {
+  image(imgF, 0, 0, width, height);
+  cx = 166;
+  cy = 589;
+  i = 1;
+  create();
+  fish.style("background-color", "#854008");
+  kitty.style("background-color", "#f67f00");
+}
+
+//checks which one is the current selection and restores its original aspect
+function deviceShaken() {
+  if (i == 0) {
+    setKitty();
+  } else {
+    setFish();
+  }
+}
+
+//asks for the permission to use the gyroscope datas
+function touchEnded(event) {
+  if (DeviceOrientationEvent && DeviceOrientationEvent.requestPermission) {
+    DeviceOrientationEvent.requestPermission();
+  }
+}
